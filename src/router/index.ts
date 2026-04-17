42e318b37ddfa6bcefea8881fc8fb57370a10fd7
Gabrieldev28 <gabrieldev28@gmail.com>
2026-04-17 00:03:55 -0300

    login iniciado

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import LoginView from '@/views/LoginView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes
+}) 
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="login-container">
+  
+  <!-- FUNDO (shape) -->
+  <div class="green-shape"></div>
+
+  <!-- CONTEÚDO (logo + texto) -->
+  <div class="left-content flex-col items-center w-2/4">
+    <img src="../assets/logo.svg" class="logo" />
+    <h3 class="text-3xl">Automatize. Organize. Converta.</h3>
+  </div>
+
+  <!-- LADO DIREITO (login) -->
+  <div class="right-content w-2/4 bg-amber-900">
+    <!-- form -->
+     <h2 class="text-5xl">login</h2>
+  </div>
+
+</div>
+</template>
+
+<style>
+.login-container {
+  overflow: hidden;
+}
+
+/* SHAPE */
+.green-shape {
+  position: absolute;
+  left: -30vw;
+  width: 70vw;
+  height: 110vh;
+
+  background: #0b1d0b;
+
+  border-radius: 200px;
+
+  transform: rotate(-36deg);
+  z-index: 1;
+}
+
+/* CONTEÚDO EM CIMA */
+.left-content {
+  position: absolute;
+  left: 5%;
+  top: 50%;
+  transform: translateY(-50%);
+  
+  z-index: 2; /* 👈 MAIS IMPORTANTE */
+}
+
+</style>
