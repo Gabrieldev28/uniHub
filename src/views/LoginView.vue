94ddbc6922e62662d94e38e08545c7fcc57b2f82
Gabrieldev28 <gabrieldev28@gmail.com>
2026-04-17 17:04:32 -0300

    posição do logo finalizado, agora iniciando o card de login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,52 +1,60 @@
 <template>
-<div class="login-container">
-  
-  <!-- FUNDO (shape) -->
-  <div class="green-shape"></div>
+  <div class="relative w-full h-screen overflow-hidden">
+    <!-- FUNDO (shape) -->
+    <div class="green-shape shadow-2xl"></div>
 
-  <!-- CONTEÚDO (logo + texto) -->
-  <div class="left-content flex-col items-center w-2/4">
-    <img src="../assets/logo.svg" class="logo" />
-    <h3 class="text-3xl">Automatize. Organize. Converta.</h3>
+    <!-- CONTEÚDO (logo + texto) -->
+    <div class="relative z-2 w-full h-screen flex justify-around items-center">
+      <div class="flex flex-col items-center justify-center w-2/6">
+        <img src="../assets/logo.svg" class="max-w-72 gap-8" />
+        <h3 class="text-2xl text-[#A6FF95]">Automatize. Organize. Converta.</h3>
+      </div>
+
+      <!-- LADO DIREITO (login) -->
+      <div class="right-content w-2/6 text-[#111601] py-10 px-20">
+        <!-- form -->
+        <div>
+          <h2 class="text-5xl font-semibold">LOGIN</h2>
+          <p class="text-2xl">Bem vindo de volta!</p>
+        </div>
+        <div>
+          <div class="p-3 flex gap-2 border-b border-[#111601]">
+            <img src="../assets/icons/mail.svg" alt="mail" />
+            <input
+              type="text"
+              placeholder="Email"
+              class="placeholder:text-[#111601] placeholder:text-base w-full outline-none"
+              name=""
+              id=""
+            />
+          </div>
+          <div>
+            <div class="p-3 flex gap-2 border-b border-[#111601] justify-between">
+              <div class="flex gap-2">
+                <img src="../assets/icons/lock.svg" alt="lock" />
+                <input type="password" placeholder="Senha" class="placeholder:text-[#111601] placeholder:text-base w-full outline-none" name="" id=""/>
+              </div>
+              <button><img src="../assets/icons/eye.svg" alt=""></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-
-  <!-- LADO DIREITO (login) -->
-  <div class="right-content w-2/4 bg-amber-900">
-    <!-- form -->
-     <h2 class="text-5xl">login</h2>
-  </div>
-
-</div>
 </template>
 
 <style>
-.login-container {
-  overflow: hidden;
-}
-
 /* SHAPE */
 .green-shape {
   position: absolute;
-  left: -30vw;
-  width: 70vw;
-  height: 110vh;
-
-  background: #0b1d0b;
-
-  border-radius: 200px;
-
+  left: -35vw;
+  width: 80vw;
+  height: 130vh;
+  background: #111601;
+  border-radius: 8rem;
   transform: rotate(-36deg);
   z-index: 1;
 }
 
 /* CONTEÚDO EM CIMA */
-.left-content {
-  position: absolute;
-  left: 5%;
-  top: 50%;
-  transform: translateY(-50%);
-  
-  z-index: 2; /* 👈 MAIS IMPORTANTE */
-}
-
 </style>
